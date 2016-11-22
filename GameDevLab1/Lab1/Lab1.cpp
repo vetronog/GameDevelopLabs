c80f9e10f97c1fe27b4e44a1ff763412e29b9056
vetronog <vetronog@yandex.ru>
2016-11-22 10:53:52 +0300

    Исправлены issues

--- GameDevLab1/Lab1/Lab1.cpp
+++ GameDevLab1/Lab1/Lab1.cpp
@@ -7,53 +7,54 @@
 
 #include "stdafx.h"
 #include <cmath>
+const float GRAVITY = 9.8f;
 
-bool AskAndCheckHeight(int &inputHeight)
+int AskAndGetHeight()
 {
+	int inputHeight;
 	printf("inputHeight: ");
 	if (0 == scanf_s("%d", &inputHeight))
 	{
 		printf("\n" "expected integer-point number" "\n");
-		return false;
+		return -1;
 	}
 	if ((inputHeight <= 0) || (inputHeight >= INT_MAX))
 	{
-		return false;
+		return -1;
 	}
-	return true;
+	return inputHeight;
 }
 
-void CalcHeight(float maxRiseTime, const float &gravityConst)
+void PrintTimeAndHeight(float maxRiseTime, float &currentHeight, float initSpeed)
+{
+	currentHeight = initSpeed * maxRiseTime - 0.5f * GRAVITY * pow(maxRiseTime, 2);
+	printf("flyTime = %f, currentHeight = %f\n", maxRiseTime, currentHeight);
+}
+
+void CalcHeight(float maxRiseTime)
 {
 	bool isJumpEnd = false;
-	float initSpeed = gravityConst * maxRiseTime;
+	float initSpeed = GRAVITY * maxRiseTime;
 	float currentHeight;
 	for (float flyTime = 0; flyTime < maxRiseTime * 2; flyTime += 0.1f)
 	{
 		if (flyTime > maxRiseTime && !isJumpEnd)
 		{
 			isJumpEnd = true;
-			currentHeight = initSpeed * maxRiseTime - 0.5f * gravityConst * pow(maxRiseTime, 2);
-			printf("flyTime=%f, currentHeight=%f\n", maxRiseTime, currentHeight);
-		}
-		currentHeight = initSpeed * flyTime - 0.5f * gravityConst * pow(flyTime, 2);
-		printf("flyTime=%f, currentHeight=%f\n", flyTime, currentHeight);
-	}
-	currentHeight = initSpeed * (maxRiseTime * 2) - 0.5f * gravityConst * pow(maxRiseTime * 2, 2);
-	printf("flyTime=%f, currentHeight=%f\n", maxRiseTime * 2, currentHeight);
+			PrintTimeAndHeight(maxRiseTime, currentHeight, initSpeed);			
+		}PrintTimeAndHeight(flyTime, currentHeight, initSpeed);
+	}PrintTimeAndHeight(maxRiseTime * 2, currentHeight, initSpeed);
 }
 
 int main(int, char *[])
 {
-	const float gravityConst = 9.8f;
-	float maxRiseTime;
-	int inputHeight;
-	if (!AskAndCheckHeight(inputHeight))
+	int inputHeight = AskAndGetHeight();
+	if (inputHeight == -1)
 	{
 		exit(1);
 	}
-	maxRiseTime = sqrt(inputHeight * 2 / gravityConst);
+	float maxRiseTime = sqrt(inputHeight * 2 / GRAVITY);
 	printf("maxRiseTime=%f\n", maxRiseTime);
-	CalcHeight(maxRiseTime, gravityConst);
+	CalcHeight(maxRiseTime);
 	return 0;
 }
